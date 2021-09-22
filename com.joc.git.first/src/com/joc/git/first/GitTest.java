4a413cc8be786e76379ce8a4a07b3f7736d11fed
jonas c <jonasxp@gmail.com>
2021-09-22 22:58:14 +0800

    step 2

--- com.joc.git.first/src/com/joc/git/first/GitTest.java
+++ com.joc.git.first/src/com/joc/git/first/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git was fun");
+		System.out.println("Git wasn't cool");
 
 	}
 
