d07b7462aa8a76b13ea17dfac0504e9db23eedc1
jonas c <jonasxp@gmail.com>
2021-09-22 22:53:58 +0800

    how about the first step

---
+++ com.joc.git.first/src/com/joc/git/first/GitTest.java
@@ -0,0 +1,11 @@
+package com.joc.git.first;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git was fun");
+
+	}
+
+}
---
+++ com.joc.git.first/src/module-info.java
@@ -0,0 +1,2 @@
+module com.joc.git.first {
+}
